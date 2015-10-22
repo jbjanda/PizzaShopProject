cb8293209fe01d2f30f812416f2d5bc6f11dcc2d
Jeff Janda <jbjanda@gmail.com>
2015-10-22 16:25:14 -0400

    Initial Commit for creation of repo

---
+++ JJanda_Assignment1/Customer.cs
@@ -0,0 +1,165 @@
+﻿/*
+ * --------------------------------------------------------------------------
+ * Developer: Jeff Janda
+ * Program Name: Assignment 2, Customer Class
+ * Date: 9/23/15
+ * ---------------------------------------------------------------------------
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace JJanda_Assignment1
+{
+    /// <summary>
+    /// The Customer class holds and reports information about a customer.
+    /// Its only methods are Constructors (default and overloaded), 
+    /// a Destructor, Property Gets/Sets, and an overriden ToString(). 
+    /// </summary>
+
+    class Customer
+    {
+        #region Fields and Properties
+        //field variable for Customer first name  
+        private string _firstName;
+
+        //FirstName property methods
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value.Trim(); }
+        }
+
+        //field variable for Customer last name
+        private string _lastName;
+
+        //LastName property methods
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value.Trim(); }
+        }
+
+        //field variable for Customer phone number
+        private string _phoneNumber;
+
+        //PhoneNumber property methods
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value.Trim(); }
+        }
+        #endregion
+
+        # region Constructor and Destructor
+        /// <summary>
+        /// Default Constructor. Creates a Customer object with empty values.
+        /// </summary>
+        public Customer()
+        {
+            _firstName = string.Empty;
+            _lastName = string.Empty;
+            _phoneNumber = string.Empty;
+        }
+
+        /// <summary>
+        /// Overloaded Customer constructor. 
+        /// </summary>
+        /// <param name="pFirstName">First Name parameter</param>
+        /// <param name="pLastName">Last Name parameter</param>
+        /// <param name="pPhoneNumber">Phone Number parameter</param>
+        public Customer(string pFirstName, string pLastName,
+                                string pPhoneNumber)
+        {
+            FirstName = pFirstName;
+            LastName = pLastName;
+            PhoneNumber = pPhoneNumber;
+        }
+        /// <summary>
+        /// Destructor; sets all attributes to null
+        /// </summary>
+        ~Customer()
+        {
+            _firstName = null;
+            _lastName = null;
+            _phoneNumber = null;
+        }
+        #endregion
+
+        /// <summary>
+        /// Creates a string representation of a Customer object formatted
+        /// as a customer information summary.
+        /// </summary>
+        /// <returns>A string representation of a Customer object</returns>
+        public override string ToString()
+        {
+            //message variable holds string information about Customer object
+            string message;
+
+            //formats Customer object information into a information summary
+            //phone number calls the FormatPhoneNumber method to format
+            //in an approved form.
+            message = "Name: " + FirstName + " " + LastName + Environment.NewLine +
+               "Phone Number: " + formatPhoneNumber(PhoneNumber);
+
+            //returns the summary
+            return message;
+        }
+
+
+        /// <summary>
+        /// Formats a phone number into the form 111-222-3333.
+        /// Assumes data from parameter is validated to contain
+        /// 10 digits.
+        /// </summary>
+        /// <param name="pPhoneNumber">Takes in string phone number validated to have 10 digits</param>
+        /// <returns>formatted phone number</returns>
+        private string formatPhoneNumber(string pPhoneNumber)
+        {
+            //variable reference to hold the formatted phone number
+            string formattedNumber;
+            //assignment of argument data to formattedNumber
+            formattedNumber = pPhoneNumber;
+
+            /*-------------------------------------------------------
+             * The following algorithm moves through the string array
+             * and replaces any character that is not a digit with 
+             * an empty string character.
+             * -----------------------------------------------------*/
+
+
+
+            try
+            {
+                foreach (char c in formattedNumber)
+                {
+                    //if character c is not a digit
+                    if (!char.IsDigit(c))
+                    {
+                        //replaces the character with empty string character
+                        formattedNumber = formattedNumber.Replace(c.ToString(), "");
+                    }
+                }
+
+                //following two statments add formatting to the phonenumber 
+                //as (111) 222-3333.
+                formattedNumber = formattedNumber.Insert(0, "(");
+                formattedNumber = formattedNumber.Insert(4, ") ");
+                formattedNumber = formattedNumber.Insert(9, "-");
+
+                //returns the formatted number.
+                return formattedNumber;
+            }
+            catch (Exception ex)
+            {
+                return "Please enter a valid phone number. " 
+                    + Environment.NewLine + ex.Message;
+            }
+
+        }
+    }
+}
---
+++ JJanda_Assignment1/FormShop.cs
@@ -0,0 +1,146 @@
+﻿/*
+ * --------------------------------------------------------------------------
+ * Developer: Jeff Janda
+ * Program Name: Assignment 1, Pizza Shop
+ * Date: 8/31/15
+ * ---------------------------------------------------------------------------
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JJanda_Assignment1
+{
+    /// <summary>
+    /// PizzaShop class Form control takes in user input from a TextBox control
+    /// and print a Label Control message stating how many pizzas were ordered
+    /// by the user. Input is validated using the TryParse method and passing the
+    /// out int variable pizzaNumber reference to the Label message. Non int
+    /// user input is assigned a value of 0.
+    /// 
+    /// Users are provided three Button controls; Order, Clear, and Exit.
+    /// Order will print the Label Control message. Clear will clear the message
+    /// and value in the Textbox. Exit will close the program. 
+    /// 
+    /// In addition to the Controls above, there are several Label controls which
+    /// are used provide orientation for the user through the GUI. 
+    /// </summary>
+    
+   
+    public partial class PizzaShop : Form
+    {
+        
+        public PizzaShop()
+        {
+            InitializeComponent();
+        }
+        /*--------------------------------------------------------
+         * Click event handler for buttonOrder control.
+         * This method takes input from the txtBoxPizzaNumber 
+         * control and prints a message in the labelMessage control
+         * stating the number of Pizzas a user has ordered. This 
+         * method also clears the txtBoxPizzaNumber control
+         * --------------------------------------------------------
+         */
+        private void buttonOrder_Click(object sender, EventArgs e)
+        {
+            //holds the value for number of pizzas ordered
+            int pizzaNumber = 0; 
+
+           
+            /*----------------------------------------------------
+             * TryParse method is being used here to provide a
+             * simple input validation by taking the value of
+             * the txtBoxPizzaNumber and assigning it to 
+             * the pizzaNumber variable if true. If false
+             * pizzaNumber remains equal to 0. 
+             * 
+             * Prevents a logical error where a user enters a value
+             * other than an a number. 
+             *----------------------------------------------------- 
+             */
+            int.TryParse(txtBoxPizzaNumber.Text, out pizzaNumber);
+
+            //Conditional statement for pizza orders greater than 1.
+            if (pizzaNumber > 1)
+            {   
+                //Order message for multiple pizzas.
+                labelOrderMessage.Text = "You ordered " + pizzaNumber + " pizzas. "
+                    + "Your order will be ready shortly.";
+            }
+            //Conditional statement for pizza orders equal to 1.
+            else if (pizzaNumber == 1)
+            {
+                //Order message for a single pizza.
+                labelOrderMessage.Text = "You ordered " + pizzaNumber + " pizza. "
+                + "Your order will be ready shortly.";
+            }
+            //conditional statement for orders of 0 or non int values.
+            else if (pizzaNumber == 0)
+            {
+                //Order message for 0 or non nummeric orders.
+                labelOrderMessage.Text = "You didn't order any pizzas.";
+            }
+
+            //clears the txtBoxPizzaNumber control.
+            txtBoxPizzaNumber.Text = "";
+        }
+        /*------------------------------------------------------------
+         * Click event handler for buttonClear control.
+         * This method clears the value from the textBoxPizzaNumber
+         * and the pizza order message.
+         * -----------------------------------------------------------
+         */
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            labelOrderMessage.Text = ""; //clears the order message
+
+            txtBoxPizzaNumber.Text = ""; //clears the Textbox control
+        }
+        /*------------------------------------------------------------
+         * Click event handler for the buttonExit control.
+         * This method calls the Form Close() and exits the program.
+         * -----------------------------------------------------------
+         */
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Event handler for customer button
+        /// </summary>
+        /// <param name="sender">control initiating event</param>
+        /// <param name="e">event argument</param>
+        private void buttonCustomer_Click(object sender, EventArgs e)
+        {
+            //creates a new FormCustomer object
+            FormCustomer customerForm = new FormCustomer();
+            
+            //opens a Customer Form
+            customerForm.ShowDialog();
+        }
+
+       /// <summary>
+       /// Event handler for Thin Crust Button
+       /// </summary>
+       /// <param name="sender">control initiating event</param>
+       /// <param name="e">event argument</param>
+        private void buttonThinCrust_Click(object sender, EventArgs e)
+        {
+            //creates a new FormThinCrust object
+            FormThinCrust thinCrustForm = new FormThinCrust();
+
+            //opens a Thin Crust Form
+            thinCrustForm.ShowDialog();
+        }
+    }
+}
